2c874cccaae232f896f10666b3cb2f3580d48ac3
Lam <dayflyking@163.com>
2020-03-12 16:21:12 +0800

    -增加新贴图功能的使用实例代码

--- LFMediaEditingController/PhotoViewController.m
+++ LFMediaEditingController/PhotoViewController.m
@@ -115,13 +115,27 @@
 
 - (void)photoEditing
 {
+//    NSArray *stickerGifs = @[[NSURL URLWithString:@"https://n.sinaimg.cn/tech/transform/677/w400h277/20200219/4639-iprtayz5721379.gif"], [NSURL URLWithString:@"https://f.sinaimg.cn/tech/transform/40/w420h420/20200214/b778-ipmxpvz6387339.gif"], [NSURL URLWithString:@"https://n.sinaimg.cn/tech/transform/362/w244h118/20200214/d095-ipmxpvz6380936.gif"], [NSURL URLWithString:@"https://n.sinaimg.cn/tech/transform/552/w315h237/20200214/75d2-ipmxpvz6380604.gif"], [NSURL URLWithString:@"https://n.sinaimg.cn/tech/transform/538/w350h188/20200214/49ef-ipmxpvz6378358.gif"], [NSURL URLWithString:@"https://n.sinaimg.cn/tech/transform/18/w536h282/20200213/256b-ipmxpvz2333375.gif"], [NSURL URLWithString:@"https://f.sinaimg.cn/tech/transform/755/w280h475/20200213/ae28-ipmxpvz2324934.gif"], [NSURL URLWithString:@"https://n.sinaimg.cn/tech/transform/704/w351h353/20200213/34b7-ipmxpvz2320937.gif"], [NSURL URLWithString:@"https://f.sinaimg.cn/tech/transform/474/w308h166/20200213/3554-ipmxpvz2313851.gif"], [NSURL URLWithString:@"https://fail.gif"]];
+//
+//    NSURL *appendFile = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"1.jpg" ofType:nil]];
+//    NSURL *failFile = [NSURL fileURLWithPath:[[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask,YES) objectAtIndex:0] stringByAppendingPathComponent:@"test"]];
+    
     LFPhotoEditingController *lfPhotoEditVC = [[LFPhotoEditingController alloc] init];
 //    lfPhotoEditVC.operationType = LFPhotoEditOperationType_draw | LFPhotoEditOperationType_splash;
 //    lfPhotoEditVC.defaultOperationType = LFPhotoEditOperationType_crop; // 默认剪裁
 //    lfPhotoEditVC.operationAttrs = @{
 //                                     LFPhotoEditDrawColorAttributeName:@(LFPhotoEditOperationSubTypeDrawVioletRedColor), // 绘画紫罗兰红色
 //                                     LFPhotoEditDrawBrushAttributeName:@(LFPhotoEditOperationSubTypeDrawStampFruitBrush), // 绘画笔刷
-////                                     LFPhotoEditStickerAttributeName:@"描述（贴图路径）",
+//                                     LFPhotoEditStickerContentsAttributeName:@[
+//                                             [LFStickerContent stickerContentWithTitle:@"默认" contents:@[
+//                                                 appendFile,
+//                                                 failFile,
+//                                                 LFStickerContentDefaultSticker]],
+//                                             [LFStickerContent stickerContentWithTitle:@"相册" contents:@[
+//                                                 LFStickerContentAllAlbum
+//                                             ]],
+//                                             [LFStickerContent stickerContentWithTitle:@"GIF" contents:stickerGifs]
+//                                                                                ], // 贴图资源
 //                                     LFPhotoEditTextColorAttributeName:@(LFPhotoEditOperationSubTypeTextAzureColor), // 字体天蓝色
 //                                     LFPhotoEditSplashAttributeName:@(LFPhotoEditOperationSubTypeSplashPaintbrush), //涂抹效果
 //                                     LFPhotoEditFilterAttributeName:@(LFPhotoEditOperationSubTypeProcessFilter), //滤镜效果
